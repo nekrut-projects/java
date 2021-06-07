27cae363c7a659b32fbf7738ad37cdbe8c7a8c77
nekrut <nekrut12@gmail.com>
2021-06-07 19:55:37 +0300

    Cloud_store_0.2

--- src/main/java/client/ClientMessageHandler.java
+++ src/main/java/client/ClientMessageHandler.java
@@ -14,50 +14,7 @@ public class ClientMessageHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        callback.call((ByteBuf) msg);
-//        ByteBuf byteBuf = (ByteBuf) msg;
-//        byte marker = byteBuf.readByte();
-//        System.out.println(marker);
-//        System.out.println("........................");
-//        if (marker == Marker.UPLOAD_FILE.getValue()) {
-//            System.out.println("Client upload file bloc");
-//            //
-//        } else if(marker == Marker.DOWNLOAD_FILE.getValue()) {
-//            System.out.println("Client download file bloc");
-//            //
-//        } else if (marker == Marker.SEND_LIST_FILES.getValue()) {
-//            System.out.println("Client input list block");
-//            ByteBuf b = (ByteBuf) msg;
-//            StringBuilder listFiles = new StringBuilder();
-////            System.out.println(b.readByte());
-//            while (b.isReadable()){
-//                listFiles.append((char) b.readByte());
-//            }
-//            System.out.println(listFiles.toString());
-//            ctx
-//            System.out.println("++++++++++++++++++++");
-////            b.writeBytes(ctx.channel());
-//            //
+         callback.call((ByteBuf) msg);
         }
     }
 
-//    @Override
-//    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-//        ByteBuf byteBuf = ctx.alloc().directBuffer();
-//        byte marker = byteBuf.readByte();
-//        System.out.println(marker);
-//        System.out.println("........................");
-//        if (marker == Marker.UPLOAD_FILE.getValue()) {
-//            System.out.println("Client upload file bloc");
-//            //
-//        } else if(marker == Marker.DOWNLOAD_FILE.getValue()) {
-//            System.out.println("Client download file bloc");
-//            //
-//        } else if (marker == Marker.SEND_LIST_FILES.getValue()) {
-//            System.out.println("Client input list block");
-//            ByteBuf b = ctx.alloc().directBuffer();
-////            b.writeBytes(ctx.channel());
-//            //
-//        }
-//    }
-//}
